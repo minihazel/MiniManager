1c2e4de63424819cb97b290e074d9f1b04d15f0d
hazelify <decyanidez@gmail.com>
2023-03-27 19:43:52 +0200

    Add project files.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace MiniManager
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.tabPanel = new System.Windows.Forms.Panel();
+            this.verticalSeparator1 = new System.Windows.Forms.Panel();
+            this.clientModsPanel = new System.Windows.Forms.Panel();
+            this.serverModsPanel = new System.Windows.Forms.Panel();
+            this.appSettingsPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tabPanel
+            // 
+            this.tabPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tabPanel.Location = new System.Drawing.Point(0, 12);
+            this.tabPanel.Name = "tabPanel";
+            this.tabPanel.Size = new System.Drawing.Size(257, 561);
+            this.tabPanel.TabIndex = 0;
+            // 
+            // verticalSeparator1
+            // 
+            this.verticalSeparator1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.verticalSeparator1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.verticalSeparator1.Location = new System.Drawing.Point(257, 0);
+            this.verticalSeparator1.Name = "verticalSeparator1";
+            this.verticalSeparator1.Size = new System.Drawing.Size(1, 586);
+            this.verticalSeparator1.TabIndex = 1;
+            // 
+            // clientModsPanel
+            // 
+            this.clientModsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clientModsPanel.Location = new System.Drawing.Point(261, 1);
+            this.clientModsPanel.Name = "clientModsPanel";
+            this.clientModsPanel.Size = new System.Drawing.Size(640, 584);
+            this.clientModsPanel.TabIndex = 2;
+            // 
+            // serverModsPanel
+            // 
+            this.serverModsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serverModsPanel.Location = new System.Drawing.Point(261, 1);
+            this.serverModsPanel.Name = "serverModsPanel";
+            this.serverModsPanel.Size = new System.Drawing.Size(640, 584);
+            this.serverModsPanel.TabIndex = 3;
+            // 
+            // appSettingsPanel
+            // 
+            this.appSettingsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.appSettingsPanel.Location = new System.Drawing.Point(261, 1);
+            this.appSettingsPanel.Name = "appSettingsPanel";
+            this.appSettingsPanel.Size = new System.Drawing.Size(640, 584);
+            this.appSettingsPanel.TabIndex = 4;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(902, 585);
+            this.Controls.Add(this.verticalSeparator1);
+            this.Controls.Add(this.tabPanel);
+            this.Controls.Add(this.clientModsPanel);
+            this.Controls.Add(this.appSettingsPanel);
+            this.Controls.Add(this.serverModsPanel);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 11F);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Manager";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tabPanel;
+        private System.Windows.Forms.Panel verticalSeparator1;
+        private System.Windows.Forms.Panel clientModsPanel;
+        private System.Windows.Forms.Panel serverModsPanel;
+        private System.Windows.Forms.Panel appSettingsPanel;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace MiniManager
+{
+    public partial class mainForm : Form
+    {
+        int default_item_height = 40; // item height
+        int default_item_loc_x = 1; // keep 1
+        int default_item_loc_y = 160; // item location startpoint
+        int default_item_spacer = 40; // spacer between item to item
+
+        public Color idleText = Color.LightGray;
+        public Color idleColor = Color.FromArgb(255, 35, 35, 40);
+        public Color hoverColor = Color.FromArgb(255, 32, 32, 35);
+        public Color selectColor = Color.FromArgb(255, 31, 31, 34);
+        public Font idleFont = new Font("Bahnschrift Light", 11, FontStyle.Regular);
+
+        public string[] tabContents = {
+            "Client mods",
+            "Server mods",
+            "App settings",
+            "Close"
+        };
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            clientModsPanel.Visible = false;
+            serverModsPanel.Visible = false;
+            appSettingsPanel.Visible = false;
+
+            default_item_loc_y = tabPanel.Size.Height / 3;
+            listSystem(tabPanel, tabContents);
+        }
+
+        private void clearUI(Panel control)
+        {
+            // server box
+            for (int i = control.Controls.Count - 1; i >= 0; i--)
+            {
+                Label selected = control.Controls[i] as Label;
+                if (selected != null)
+                {
+                    try
+                    {
+                        control.Controls.RemoveAt(i);
+                        selected.Dispose();
+                    }
+                    catch (Exception err)
+                    {
+                        Debug.WriteLine($"ERROR: {err.ToString()}");
+                        MessageBox.Show($"Oops! It seems like we received an error. If you're uncertain what it\'s about, please message the developer with a screenshot:\n\n{err.ToString()}", this.Text, MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        public void listSystem(Control component, string[] arr)
+        {
+            clearUI(tabPanel);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = arr[i];
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(component.Size.Width - 2, default_item_height);
+                lbl.Location = new Point(default_item_loc_x, default_item_loc_y + (i * default_item_spacer));
+                lbl.Font = idleFont;
+                lbl.BackColor = idleColor;
+                lbl.ForeColor = idleText;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                lbl.Visible = true;
+                component.Controls.Add(lbl);
+            }
+        }
+
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = hoverColor;
+                }
+            }
+        }
+
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = idleColor;
+                }
+            }
+        }
+
+        private void lbl_MouseUp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "") { }
+        }
+
+        private void lbl_MouseDown(object sender, EventArgs e)
+        {
+            Control list = this.Controls["tabPanel"];
+
+            clientModsPanel.Visible = true;
+            serverModsPanel.Visible = true;
+            appSettingsPanel.Visible = true;
+
+            System.Windows.Forms.Label lbl = (System.Windows.Forms.Label)sender;
+
+            if (lbl.Text != "")
+            {
+                foreach (Control component in list.Controls)
+                {
+                    if (component.Text.Contains("> "))
+                    {
+                        component.Text = component.Text.Remove(0, 2);
+                    }
+
+                    if (component is Label && component.Text != lbl.Text)
+                    {
+                        component.BackColor = idleColor;
+                        component.ForeColor = idleText;
+                    }
+                }
+
+                lbl.BackColor = selectColor;
+
+                if (lbl.Text.ToLower() == "client mods")
+                {
+                    clientModsPanel.BringToFront();
+
+
+                }
+                else if (lbl.Text.ToLower() == "server mods")
+                {
+                    serverModsPanel.BringToFront();
+
+
+                }
+                else if (lbl.Text.ToLower() == "app settings")
+                {
+                    appSettingsPanel.BringToFront();
+
+
+                }
+                else if (lbl.Text.ToLower() == "close")
+                {
+                    Application.Exit();
+                }
+
+                string activeItem = lbl.Text;
+                activeItem = "> " + activeItem;
+                lbl.Text = activeItem;
+                lbl.ForeColor = Color.DodgerBlue;
+            }
+        }
+    }
+}
